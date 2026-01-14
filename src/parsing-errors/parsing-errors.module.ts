326d896d23052be889bdc65b15f5ddc99e159c61
KotoFeelGood72 <kotenkolove@yandex.ru>
2026-01-14 23:22:56 +0300

    ss

--- src/parsing-errors/parsing-errors.module.ts
+++ src/parsing-errors/parsing-errors.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ParsingErrorsController } from './parsing-errors.controller';
 import { ParsingErrorsService } from './parsing-errors.service';
 import { ParsingError } from './entities/parsing-error.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ParsingError])],
+  imports: [
+    TypeOrmModule.forFeature([ParsingError]),
+    UsersModule,
+  ],
   controllers: [ParsingErrorsController],
   providers: [ParsingErrorsService],
   exports: [ParsingErrorsService],
